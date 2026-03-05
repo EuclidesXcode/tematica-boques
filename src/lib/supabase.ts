49ae733ac379b6e9577b558a83588a5a9c642830
Eucode <eucode@MacBook-Air-de-Eucode.local>
2026-03-04 23:31:47 -0300

    changes on teste

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,8 +1,8 @@
 import { createBrowserClient } from '@supabase/ssr'
 
 export function createClient() {
-  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-  const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || "https://fidbarjoswfggjpipmuv.supabase.co";
+  const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZpZGJhcmpvd3dmZ2dqcGlwbXUidiIsInJvbGUiOiJhbm9uIiwiaWF0IjoxNzQxMTE4NDQ0LCJleHAiOjIwNTY2OTQ0NDR9.5681067952141111111111111111111111111111111111111111111111111111";
 
   if (!supabaseUrl || !supabaseKey) {
     // Retorna um cliente "vazio" ou loga o aviso durante o build da Vercel
